44b07079d9e8baaba592a0d4ef42eeca146a4a9f
Aigerim <aigerabasheva@gmail.com>
2024-12-06 09:02:06 +0500

    Added Student

---
+++ Java-basics/src/Student.java
@@ -0,0 +1,20 @@
+// 1.
+public class Student {
+    String name;
+    String surname;
+    String address;
+    int id;
+
+    public Student(String name,String surname, String address, int id) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.id = id;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Студент: %s %s, адрес: %s, ID: %d", name, surname, address, id);
+    }
+}
